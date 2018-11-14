8e8df9b49ce602cd0ca04bc4fe76b2cc38f6125f
samternent <sam.ternent@gmail.com>
2018-11-14 16:18:44 +0000

    titles

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
   <section class="container">
-    <nuxt-link to="rehash-trash">
-      Rehash Trash
-    </nuxt-link>
-    <nuxt-link to="fauna-in-focus">
-      Fauna In Focus
+    <h1>Neural Network Football Result Predictions</h1>
+    <nuxt-link to="13">
+      Gameweek 13
     </nuxt-link>
   </section>
 </template>
